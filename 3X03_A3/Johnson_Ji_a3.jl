738da888fe6f83fc24059aaddec30aa5ef85b6ab
Albert Ludwig <johnsonji0602@gmail.com>
2025-12-01 17:30:41 -0500

    Assignment 3

--- 3X03_A3/Johnson_Ji_a3.jl
+++ 3X03_A3/Johnson_Ji_a3.jl
@@ -18,7 +18,7 @@ function horner(c, x, X)
     for k = 1:m
         value = c[n] # initial with cn
         for i = n-1:-1:1
-            value = value * (X[k] - x[i]) + c[i]
+            value = value * (X[k] - x[i]) + c[i] # evaluate using Horner's method, use the nested structure.
         end
         p[k] = value
     end
@@ -28,31 +28,31 @@ end
 #Problem 3
 function composite_trapezoidal_rule(f, a, b, r)
     h = (b - a) / r # step size
-    sum_val = f(a) + f(b)
+    sum_val = f(a) + f(b) # upper base + lower base of trapezoid
     for i = 1:(r-1)
-        sum_val += 2 * f(a + i * h)
+        sum_val += 2 * f(a + i * h) # use the iteration to sum the whole interval
     end
     return (h / 2) * sum_val
 end
 
 function composite_midpoint_rule(f, a, b, r)
-    h = (b - a) / r
+    h = (b - a) / r # step size
     sum_val = 0.0
     for i = 1:r
-        midpoint = a + (i - 0.5) * h
-        sum_val += f(midpoint)
+        midpoint = a + (i - 0.5) * h # midpoint of each subinterval
+        sum_val += f(midpoint) # sum the function values at midpoints
     end
-    return h * sum_val
+    return h * sum_val # the whole rectangle area
 end
 
 function composite_simpsons_rule(f, a, b, r)
     # note the r is even
-    h = (b - a) / r
+    h = (b - a) / r # step size
     sum_val = f(a) + f(b)
-    for i = 1:2:(r-1)
+    for i = 1:2:(r-1) # first half interval
         sum_val += 4 * f(a + i * h)
     end
-    for i = 2:2:(r-2)
+    for i = 2:2:(r-2) # second half interval
         sum_val += 2 * f(a + i * h)
     end
     return (h / 3) * sum_val
